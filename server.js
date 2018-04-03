9787c92e165567fcb0ed1971bf0ddea01fd27b09
ronnie1734 <rlewwwis@yahoo.com>
2018-04-02 21:03:48 -0700

    setup env and server connection

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
+'use strict'
+
+require('dotenv').config();
+
 const express = require('express');
 const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
 const db = require('./config/db');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 
-
-const port = 3000
+const PORT = process.env.PORT
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
+app.listen(PORT, () => {
+  console.log('Tuning in to port ' + PORT);
+});
 
-  db = database.db("starshipmanager");
-  require('./app/routes') (app, db);
+// MongoClient.connect(db.url, (err, database) => {
+//   if (err) return console.log(err)
+
+//   db = database.db("starshipmanager");
+//   require('./app/routes') (app, db);
   
-  app.listen(port, () => {
-    console.log('Server is live on port ' + port);
-  });
-})
+//   app.listen(port, () => {
+//     console.log('Server is live on port ' + port);
+//   });
+// })
