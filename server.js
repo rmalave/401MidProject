5900c4045b6e826612201334ce5d928ca46f3b92
ronnie1734 <rlewwwis@yahoo.com>
2018-04-02 21:19:17 -0700

    add simple hello route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
 const db = require('./config/db');
 const mongoose = require('mongoose');
+const router = require('./app/routes/index');
 
 const app = express();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use('/api', router);
 
 const PORT = process.env.PORT
 
