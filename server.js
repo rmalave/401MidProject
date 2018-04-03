8f2e6c8fba3f7c34c587aee93263c817bf7f42fe
ronnie1734 <rlewwwis@yahoo.com>
2018-04-02 21:06:14 -0700

    server js change

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
-
 const app = express();
 
-const PORT = process.env.PORT
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log('Tuning in to port ' + PORT);
