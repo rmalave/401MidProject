43d4bfec4205e62610f4023037254620206828b1
Rafael Malave <rafomalave@gmail.com>
2018-04-03 18:42:41 -0700

    added basic ship model and get and post routes

---
+++ app/models/ship.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const shipSchema = new mongoose.Schema({
+   name: { type: String, required: true, unique: true }
+});
+
+module.exports = mongoose.model('Ship', shipSchema);
