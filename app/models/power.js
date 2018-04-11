2a6b88056c51773aee95d2a76e6c4bdd5256da91
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2018-04-10 19:40:31 -0700

    edited out merge conflict from power.js, supply.js, power_routes.js and HTML

--- app/models/power.js
+++ app/models/power.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose');
 
 const powerSchema = mongoose.Schema({
-<<<<<<< HEAD
-<<<<<<< HEAD
-  ship: {type: mongoose.Schema.Types.ObjectId, ref: 'Power'},
-=======
->>>>>>> 14feac5bbb091f407fce6bf558bf28cb7e1a8b03
-  battery: {type: String, require: true},
-  engineLevel: {type: String, require: true},
-=======
+
   ship: {type: mongoose.Schema.Types.ObjectId, ref: 'Power'},
   battery: {type: Number, min: 0, max: 100, require: true},
-  engineLevel: {type: Number, min: 0, max: 100, require: true},
->>>>>>> 528fe527d4a923d48a5b6133de458934753b2c4b
-})
+  engineLevel: {type: Number, min: 0, max: 100, require: true}
+});
 const Power = mongoose.model('Power', powerSchema);
 
 module.exports = Power;
